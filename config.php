54eae6b2a92ca091a5fde12bb0d3a34c3f7c1933
Rukhaiah <141475228+Rukhaiah@users.noreply.github.com>
2024-04-30 12:52:20 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,15 +1,10 @@
 <?php 
 // DB credentials.
-
-$server = "bloodserver.mysql.database.azure.com";
-$userid ="blood";
-$Password = "RukkuSreya@2024";
-$myDB = "bloodbank";
-$con = mysqli_connect($server,$userid,$Password,$myDB);
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','bbdms');
 // Establish database connection.
-if (mysqli_connect_errno()) {
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
